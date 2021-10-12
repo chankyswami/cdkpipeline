c2b54ca8d4c2c05851e206fe552dbab86f75e72d
EC2 Default User <ec2-user@ip-10-201-72-62.eu-west-1.compute.internal>
2021-10-12 06:44:05 +0000

    Initial commit of pipeline and VPC infra creation

--- app.py
+++ app.py
@@ -10,10 +10,15 @@ from aws_cdk import core as cdk
 from aws_cdk import core
 
 from pipelines_webinar.pipelines_webinar_stack import PipelinesWebinarStack
+from pipelines_webinar.pipeline_stack import PipelineStack
 
 
 app = core.App()
-PipelinesWebinarStack(app, "PipelinesWebinarStack",
+PipelinesWebinarStack(app, "Pipelines-Webinar"),
+PipelineStack(app, 'PipelineStack', env={
+  'account': '931187157570',
+  'region': 'eu-west-1',
+})
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -26,9 +31,9 @@ PipelinesWebinarStack(app, "PipelinesWebinarStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=core.Environment(account='123456789012', region='us-east-1'),
+    #env=core.Environment(account='931187157570', region='eu-west-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+#    )
 
 app.synth()
